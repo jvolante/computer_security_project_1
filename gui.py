c79fce6052eaae1df6fc27e80ba7e382c9cf91c5
McCode <jonnywoh@live.com>
2016-02-01 20:07:45 -0500

    Added (nonfunctional) GUI

--- gui.py
+++ gui.py
@@ -1,1 +1,20 @@
-from PyQt4 import Qt, QtGui
+import sys
+from PyQt4 import Qt, QtGui, QtCore
+from ui import Ui_MainWindow
+
+
+class DecrypterWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(DecrypterWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    win = DecrypterWindow()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
